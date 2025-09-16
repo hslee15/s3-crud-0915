7321f58635594ad8caf53e8576e4c18c988ec995
hslee15 <lhs01019@kbu.ac.kr>
2025-09-16 21:16:50 +0900

    0916

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import UploadForm from './components/UploadForm';
+import FileList from './components/FileList';
+
+function App() {
+  return (
+    <div className='container'>
+      <UploadForm />
+      <FileList />
+    </div>
+  );
+}
+
+export default App;
