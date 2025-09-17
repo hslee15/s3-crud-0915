90769e1f1a8b96564fa30245944b07cca69aec5d
hslee15 <lhs01019@kbu.ac.kr>
2025-09-17 20:21:19 +0900

    ㅈㅈ

--- frontend/src/components/FileList.jsx
+++ frontend/src/components/FileList.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React,{
+    useEffect,
+    useState,
+    forwardRef,
+    useImperativeHandle
+} from 'react'
 import "./FileList.scss"
-const FileList = () => {
+import api from '../api'
+
+const FileList = forwardRef(({ref}) => {
+
+    const [items, setItems]=useState([])
+
+    const load=async()=>{
+        const {data}=await api.get('/files',{
+            params:{t:Date.now()}
+        })
+
+        setItems(data)
+    }
+
+    useEffect(()=>{
+        load()
+    },[])
+
+    useImperativeHandle(ref,()=>({load}))
+
     return (
         <ul className='file-list'>
-            <li>
+            {items.map(it=>(
+
+                <li>
                 <h3>샘플 이미지</h3>
                 <div className="img-wrap">
                     <img src="https://ky-s3-crud2-0915.s3.ap-northeast-2.amazonaws.com/uploads/1757936555204-G5JnXr-test.png" alt="image" />
@@ -16,34 +42,9 @@ const FileList = () => {
                     <button className='delete-btn'>Delete</button>
                 </div>
             </li>
-            <li>
-                <h3>샘플 이미지</h3>
-                <div className="img-wrap">
-                    <img src="https://ky-s3-crud2-0915.s3.ap-northeast-2.amazonaws.com/uploads/1757936555204-G5JnXr-test.png" alt="image" />
-                </div>
-                <p>설명 샘플 입니다.</p>
-                <div className="btn-wrap">
-
-                    <a className='open-btn' href="https://ky-s3-crud2-0915.s3.ap-northeast-2.amazonaws.com/uploads/1757936555204-G5JnXr-test.png">
-                        Open</a>
-                    <button className='delete-btn'>Delete</button>
-                </div>
-            </li>
-            <li>
-                <h3>샘플 이미지</h3>
-                <div className="img-wrap">
-                    <img src="https://ky-s3-crud2-0915.s3.ap-northeast-2.amazonaws.com/uploads/1757936555204-G5JnXr-test.png" alt="image" />
-                </div>
-                <p>설명 샘플 입니다.</p>
-                <div className="btn-wrap">
-
-                    <a className='open-btn' href="https://ky-s3-crud2-0915.s3.ap-northeast-2.amazonaws.com/uploads/1757936555204-G5JnXr-test.png">
-                        Open</a>
-                    <button className='delete-btn'>Delete</button>
-                </div>
-            </li>
+            ))}
         </ul>
     )
-}
+})
 
 export default FileList
